34b72e47ff8d622bcd47b54d84bcc50209fd0221
Anton Verinov <git@zemlan.in>
2015-05-21 12:26:57 +0300

    touch navigation

--- src/main.js
+++ src/main.js
@@ -8,16 +8,32 @@ if (metaPage) { slides(metaPage.content) }
 
 const [LEFT, UP, RIGHT, DOWN] = [37, 38, 39, 40]
 Rx.Observable.fromEvent(document.body, 'keyup')
-  .pluck('keyCode')
-  .map(keyCode => {
+  .map(e => ({keyCode: e.keyCode, touch: false}))
+  .merge(
+    Rx.Observable.fromEvent(document, 'touchstart')
+      .map(e => e.touches[0])
+      .flatMap(touch => Rx.Observable.fromEvent(document, 'touchend')
+        .takeUntil(Rx.Observable.fromEvent(document, 'touchmove'))
+        .take(1)
+        .map(touch)
+      )
+      .map(touch => {
+        var keyCode
+        if (touch.clientX < document.body.clientWidth / 4) { keyCode = LEFT }
+        if (touch.clientX > 3 * document.body.clientWidth / 4) { keyCode = RIGHT }
+
+        return {keyCode, touch: true}
+      })
+  )
+  .map(({keyCode, touch}) => {
     switch (keyCode) {
       case LEFT:
-        return document.querySelector('meta[property=prev]')
+        return {meta: document.querySelector('meta[property=prev]'), touch}
       case RIGHT:
-        return document.querySelector('meta[property=next]')
+        return {meta: document.querySelector('meta[property=next]'), touch}
       default:
         return
     }
   })
-  .filter(meta => meta)
-  .subscribe(meta => location.href = meta.content)
+  .filter(v => v && v.meta)
+  .subscribe(({meta, touch}) => location.href = meta.content + (touch ? '#touch' : ''))
