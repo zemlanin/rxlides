4c1d24a3f4b3872bf49597a6d1c06fe899b40561
Anton Verinov <git@zemlan.in>
2015-05-21 10:52:14 +0300

    keyboard_demo in DOM for future mobile friendliness

--- src/slides/keyboard_demo.js
+++ src/slides/keyboard_demo.js
@@ -1,54 +1,38 @@
 import Rx from 'rx'
 import range from 'lodash.range'
 
-import {wrapToDisplay, renderStream, accumutate} from '../vision'
-
 const [UP, DOWN] = [38, 40]
 const [Q, W, E, A, S, D] = [81, 87, 69, 65, 83, 68]
-const keyCanvasData = {
-  [Q]: ['Q', 1, 1],
-  [W]: ['W', 111, 1],
-  [E]: ['E', 221, 1],
-  [A]: ['A', 41, 111],
-  [S]: ['S', 151, 111],
-  [D]: ['D', 261, 111],
+const keyIds = {
+  [Q]: 'wasd_q',
+  [W]: 'wasd_w',
+  [E]: 'wasd_e',
+  [A]: 'wasd_a',
+  [S]: 'wasd_s',
+  [D]: 'wasd_d',
 }
 
 export default () => {
-  var canvas = document.querySelector('canvas')
-  var ctx = canvas.getContext("2d")
-  ctx.strokeStyle = "#00a500"
-  ctx.fillStyle = ctx.strokeStyle
-  ctx.font = document.body.style.font
-
   var keys = Rx.Observable.fromEvent(document.body, 'keydown').pluck('keyCode')
   var startStream = keys.filter(keyCode => keyCode === Q)
   var stopStream = keys.filter(keyCode => keyCode === E)
 
-  var drawToCanvas = (keyCode) => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-
-    for (var key in keyCanvasData) {
-      ctx.strokeRect(keyCanvasData[key][1], keyCanvasData[key][2], 100, 100)
-      ctx.fillText(keyCanvasData[key][0], keyCanvasData[key][1]+20, keyCanvasData[key][2]+40)
+  var setActive = (keyCode) => {
+    for (var keySpan of document.querySelectorAll('.wasd.active')) {
+      keySpan.classList.remove('active')
     }
 
-    if (keyCode && keyCanvasData[keyCode]) {
-      ctx.fillRect(keyCanvasData[keyCode][1], keyCanvasData[keyCode][2], 100, 100)
-      ctx.fillStyle = "#DFFEE3"
-      ctx.fillText(keyCanvasData[keyCode][0], keyCanvasData[keyCode][1]+20, keyCanvasData[keyCode][2]+40)
-      ctx.fillStyle = ctx.strokeStyle
+    if (keyCode) {
+      document.getElementById(keyIds[keyCode]).classList.add('active')
     }
   }
 
-  drawToCanvas()
-
-  Rx.Observable.fromEvent(document.body, 'keyup').map(null).subscribe(drawToCanvas)
+  Rx.Observable.fromEvent(document.body, 'keyup').map(null).subscribe(setActive)
   startStream
     .map(q => keys.startWith(q).takeUntil(stopStream))
     .switch()
-    .filter(keyCode => keyCanvasData[keyCode])
-    .subscribe(drawToCanvas)
+    .filter(keyCode => keyIds[keyCode])
+    .subscribe(setActive)
 
   var parts = document.querySelectorAll('[id^=part_]').length
 
