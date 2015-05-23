df96ec25c36deef7b645cd4b9fa1e93faeefb243
Anton Verinov <git@zemlan.in>
2015-05-23 18:28:04 +0300

    simpler logic selector

--- src/slides/index.js
+++ src/slides/index.js
@@ -1,21 +1,24 @@
+import zipObject from 'lodash.zipobject'
+
 import mapFilterFlatmap from './map_filter_flatmap.js'
 import keyboardDemo from './keyboard_demo.js'
 import singleSubscribe from './single_subscribe.js'
 import singleCallback from './single_callback.js'
 
 const slides = [
-  'index',
-  'ui',
-  'single_callback',
-  'microsoft_xhr',
-  'callbacks_sync',
-  'single_subscribe',
-  'map_filter_flatmap',
-  'keyboard_demo',
+  ['index', null],
+  ['ui', null],
+  ['single_callback', singleCallback],
+  ['microsoft_xhr', null],
+  ['callbacks_sync', null],
+  ['single_subscribe', singleSubscribe],
+  ['map_filter_flatmap', mapFilterFlatmap],
+  ['keyboard_demo', keyboardDemo],
 ]
 
 export var prevSlide = slide => {
-  var slideIndex = slides.indexOf(slide)
+  var slidesName = slides.map(s => s[0])
+  var slideIndex = slidesName.indexOf(slide)
 
   switch (slideIndex) {
     case -1:
@@ -24,37 +27,26 @@ export var prevSlide = slide => {
     case 1:
       return '../index.html'
     default:
-      return `./${slides[slideIndex - 1]}.html`
+      return `./${slidesName[slideIndex - 1]}.html`
   }
 }
 
 export var nextSlide = slide => {
-  var slideIndex = slides.indexOf(slide)
+  var slidesName = slides.map(s => s[0])
+  var slideIndex = slidesName.indexOf(slide)
 
   switch (slideIndex) {
     case -1:
-    case slides.length - 1:
+    case slidesName.length - 1:
       return
     case 0:
-      return `./slides/${slides[1]}.html`
+      return `./slides/${slidesName[1]}.html`
     default:
-      return `./${slides[slideIndex + 1]}.html`
+      return `./${slidesName[slideIndex + 1]}.html`
   }
 }
 
 export var slideLogic = slide => {
-  switch (slide) {
-    case "map_filter_flatmap":
-      mapFilterFlatmap()
-      break;
-    case "keyboard_demo":
-      keyboardDemo()
-      break;
-    case "single_subscribe":
-      singleSubscribe()
-      break;
-    case "single_callback":
-      singleCallback()
-      break;
-  }
+  var logic = zipObject(slides)[slide]
+  if (logic) { logic() }
 }
