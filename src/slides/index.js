3dd6eadd8d664aac07cd5b4f9714ea334798389c
Anton Verinov <git@zemlan.in>
2015-05-23 17:07:40 +0300

    slides/single_callback

--- src/slides/index.js
+++ src/slides/index.js
@@ -1,6 +1,7 @@
 import mapFilterFlatmap from './map_filter_flatmap.js'
 import keyboardDemo from './keyboard_demo.js'
 import singleSubscribe from './single_subscribe.js'
+import singleCallback from './single_callback.js'
 
 export default page => {
   switch (page) {
@@ -12,6 +13,9 @@ export default page => {
       break;
     case "single_subscribe":
       singleSubscribe()
+      break;
+    case "single_callback":
+      singleCallback()
       break;
   }
 }
