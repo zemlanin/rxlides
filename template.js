2dd6e8a6dc3622f8df91ef362c8c20aaee4d9abc
Anton Verinov <git@zemlan.in>
2015-07-08 18:40:47 +0300

    named template parts

--- template.js
+++ template.js
@@ -1,17 +1,30 @@
+/*eslint-env node*/
 var mustache = require('mustache')
 var fs = require('fs')
 var base = fs.readFileSync('templates/base.html', 'utf8')
 var templates = fs.readdirSync('templates/')
 
+var slideName
+var tmplTokens
+var tmplData
+
 for (var i = 0; i < templates.length; i++) {
-  if (templates[i] !== 'base.html') {
-    var content = fs.readFileSync('templates/' + templates[i], 'utf8').split("###")
-    var view = {
-      name:templates[i].split('.')[0],
-      style:content[0],
-      content:content[1],
+  slideName = templates[i].split('.')[0]
+
+  if (slideName !== 'base') {
+    tmplTokens = fs.readFileSync('templates/' + templates[i], 'utf8')
+      .split(/### *([a-z_]+) */ig)
+      // split(regex) adds an empty string if splited string starts with a match
+      .splice(1)
+
+    tmplData = { name: slideName }
+    for (var j = 0; j < tmplTokens.length; j += 2) {
+      tmplData[tmplTokens[j].trim()] = tmplTokens[j + 1].trim()
     }
-    writeToFile(mustache.render(base, view), templates[i].split('.')[0])
+    writeToFile(
+      mustache.render(base, tmplData),
+      slideName
+    )
   }
 }
 
